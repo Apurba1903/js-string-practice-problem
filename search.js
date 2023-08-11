36a24772953ad2fc2087a90d1881522553067e94
Apurba1903 <apurba1903@gmail.com>
2023-08-11 14:43:02 +0600

    first commit

---
+++ search.js
@@ -0,0 +1,6 @@
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Apurba1903/js-string-practice-problem.git
+git push -u origin main
