bcea5fb55755ce026bde03c87e8f61754af7ac67
Apurba1903 <apurba1903@gmail.com>
2023-08-11 15:18:03 +0600

    string

--- search.js
+++ search.js
@@ -1,6 +1,37 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Apurba1903/js-string-practice-problem.git
-git push -u origin main
+const lyrics = 'Tumi bondhu kala pakhi ami jeno ki. boshonto kale tomay bolte pari ni. shada shada kala kala';
+
+const searchString = 'shAda';
+
+// const lyricsLowerCase = lyrics.toLowerCase();
+// const searchLowerCase = searchString.toLowerCase();
+// const doesExist = lyricsLowerCase.includes(searchLowerCase);
+
+const doesExist = lyrics.toLowerCase().includes(searchString.toLowerCase());
+
+console.log(doesExist);
+
+
+
+// Index Of in String
+console.log(lyrics.indexOf('shada'));
+
+const givenWord = 'shada';
+if(lyrics.indexOf(givenWord) !== -1 ){
+    console.log('Exist inside the String');
+}
+else{
+    console.log('Does not exist inside the String');
+}
+
+// StartsWith in String
+
+console.log(lyrics.startsWith('Tumi'));
+
+
+// EndsWith in String
+
+
+const fileName = 'mybiodata.pdf'
+const otherFile = 'mypic.png'
+
+fileName.endsWith('.pdf');
